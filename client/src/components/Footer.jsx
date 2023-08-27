35c3fb5f2480161baf09e2db79c20f0bbdb25d31
Byekibe <kibetpeter95@gmail.com>
2023-08-28 01:42:42 +0300

    Add social media links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom";
+import { BsFacebook, BsInstagram, BsTiktok, BsTwitter } from 'react-icons/bs';
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="row">
                 <div className="col-12 col-sm-12 col-md-6 col-lg-6 footer-logo">
+                    <div className="socials">
+                        <ul>
+                            <li><BsFacebook /></li>
+                            <li><BsInstagram /></li>
+                            <li><BsTiktok /></li>
+                            <li><BsTwitter /></li>
+                        </ul>
+                    </div>
                     <Link className="links" to="/">Diet&Nutrition</Link>
                     <div className="copyright">
                         &copy; 2023 <Link className="links" to="/">Diud. All Rights Reserved</Link>
